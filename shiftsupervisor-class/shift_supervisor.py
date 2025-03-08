ce2b74401aa3df171ee49d48894e2618f50b929d
Rylee Leavitt <ryleeleavitt@westernwyoming.edu>
2025-03-08 03:43:46 +0000

    done

--- shiftsupervisor-class/shift_supervisor.py
+++ shiftsupervisor-class/shift_supervisor.py
@@ -7,34 +7,48 @@
 #Use comments liberally throughout the program.
 
 # shift_supervisor.py
-# Import the Employee class
+
+# Import the Employee class from employee.py
 from employee import Employee
 
 # ShiftSupervisor class (inherits from Employee)
 class ShiftSupervisor(Employee):
     def __init__(self, name, number, annual_salary, annual_bonus):
         # Call the Employee initializer
+        #initializes name, number, annual_salary, and annual_bonus
+
         super().__init__(name, number)
+        #reuse the Employee's initialization code
+
         self.__annual_salary = annual_salary
+        # takes self.__annual_salary and 
+        # assigns it the value provided as the argument annual_salary
+
         self.__annual_bonus = annual_bonus
+        # takes self.__annual_bonus and 
+        # assigns it the value provided as the argument annual_bonus
 
     # Accessor methods
     def get_annual_salary(self):
         return self.__annual_salary
+        #returns the annual salary
 
     def get_annual_bonus(self):
         return self.__annual_bonus
+        #returns the annual bonus
 
-    # Mutator methods
     def set_annual_salary(self, annual_salary):
         self.__annual_salary = annual_salary
+        #sets annnual salary
 
     def set_annual_bonus(self, annual_bonus):
         self.__annual_bonus = annual_bonus
+        #sets annual bonus
 
-    # Method to calculate total compensation
+    # calculate total compensation
     def calculate_total_compensation(self):
         return self.__annual_salary + self.__annual_bonus
+        #returns the sum of annual salary + the annual bonus = total compensation
 
     # String representation
     def __str__(self):
